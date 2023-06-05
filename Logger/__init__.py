bf050e65e1bff1e1cc512ea72cff3b8a97f03295
mi.alemasov <mi.alemasov@jet.su>
2023-06-06 00:40:40 +0300

    Добавил логи в файл

--- Logger/__init__.py
+++ Logger/__init__.py
@@ -3,6 +3,7 @@
 import logging
 # модуль питона
 import sys
+from logging.handlers import RotatingFileHandler
 
 LOG_FILE_MAX_SIZE = 1024 * 1024 * 1024
 LOG_FILE_MAX_BACKUP_COUNT = 5
@@ -17,6 +18,15 @@ formatter = logging.Formatter('[%(levelname)+8s]'
 
 # стандартное добавление в логи по тому как мы настроили выше
 console_handler = logging.StreamHandler(sys.stderr)
-console_handler.setLevel(logging.INFO)
+console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
+file_handler = RotatingFileHandler(filename='Logs/bot.log',
+                                   maxBytes=1048576,
+                                   backupCount=10,
+                                   encoding='utf-8',
+                                   mode='w')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
